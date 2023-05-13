707caa5700c07595c1d01ae9eadc768b8aa56e4a
hadipsy27 <hadipsy27@gmail.com>
2023-05-13 16:48:01 +0700

    Load Balancing with Eureka, Open Feign and Spring Cloud LoadBalancer

--- employee-service/src/main/java/com/lab/employeeservice/service/APIClient.java
+++ employee-service/src/main/java/com/lab/employeeservice/service/APIClient.java
@@ -7,7 +7,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(url = "http://localhost:8080", value = "DEPARTMENT-SERVICE")
+// Implement Load Balancer
+// Change url to Eureka Application name
+@FeignClient(name = "DEPARTMENT-SERVICE")
 public interface APIClient {
     // Build Get Department By Code REST API
     @GetMapping("/api/departments/{department-code}")
