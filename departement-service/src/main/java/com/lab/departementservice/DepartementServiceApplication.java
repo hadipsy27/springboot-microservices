b54e361b63e4943634e81ac3acf7a562ac4cf18a
hadipsy27 <hadipsy27@gmail.com>
2023-05-13 14:27:38 +0700

    Register DepartmentService microservice as Eureka Client

--- departement-service/src/main/java/com/lab/departementservice/DepartementServiceApplication.java
+++ departement-service/src/main/java/com/lab/departementservice/DepartementServiceApplication.java
@@ -2,8 +2,10 @@ package com.lab.departementservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class DepartementServiceApplication {
 
 	public static void main(String[] args) {
