1b8fdc90145a6eab21a56b20270ce2e5c27124e8
hadipsy27 <hadipsy27@gmail.com>
2023-10-26 21:51:15 +0700

    Department Swagger - Customizing Swagger API Documentation using Annotations

--- departement-service/src/main/java/com/lab/departementservice/controller/DepartmentController.java
+++ departement-service/src/main/java/com/lab/departementservice/controller/DepartmentController.java
@@ -2,17 +2,32 @@ package com.lab.departementservice.controller;
 
 import com.lab.departementservice.dto.DepartmentDto;
 import com.lab.departementservice.service.DepartmentService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+@Tag(
+        name = "Deparment service - DepartmentController",
+        description = "Department controller exposes REST APIs for Department-service"
+)
 @RestController
 @RequestMapping("/api/departments")
 @AllArgsConstructor
 public class DepartmentController {
     private DepartmentService departmentService;
 
+    @Operation(
+            summary = "Save Department REST API",
+            description = "Save Department REST API is used to save department object in Database"
+    )
+    @ApiResponse(
+            responseCode = "201",
+            description = "HTTP Status Created"
+    )
     // Build Save Department Save REST API
     @PostMapping
     public ResponseEntity<DepartmentDto> saveDepartment(@RequestBody DepartmentDto departmentDto) {
@@ -20,6 +35,14 @@ public class DepartmentController {
         return new ResponseEntity<>(saveDepartment, HttpStatus.CREATED);
     }
 
+    @Operation(
+            summary = "GET Department REST API",
+            description = "GET Department REST API is used to save department object in Database"
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "HTTP Status SUKSES"
+    )
     // Build Get Department By Code REST API
     @GetMapping("/{department-code}")
     public ResponseEntity<DepartmentDto> getDepartmentByCode(@PathVariable("department-code") String departmentCode) {
